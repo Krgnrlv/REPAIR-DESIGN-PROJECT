b14d90c2c0fafbb26ec7848bd290f33e2b6c519f
Kira <120910477+Krgnrlv@users.noreply.github.com>
2023-03-28 20:31:20 +0500

    Add files via upload
    Добавлен слайдер  для десктопной и мобильной версий

--- main.js
+++ main.js
@@ -30,9 +30,9 @@ const prepareCurrentSlide=ind=>{
 };
 const nextSlide=()=>{
   if (index === slides.length - 1){
-      index=0;
+    index=0;
   }else{
-  index++;
+    index++;
   } prepareCurrentSlide(index);
 };
 const prevSlide=()=>{
@@ -56,3 +56,65 @@ links.forEach((el,indexdLink)=>{
 })
 next.addEventListener('click', nextSlide);
 prev.addEventListener('click', prevSlide);
+
+const prevMobileProject=document.getElementById('btn-prev-mobile-project'),
+    nextMobileProject=document.getElementById('btn-next-mobile-project'),
+    slideMobProjects=document.querySelectorAll('.slide-mobile-project');
+let indexMobProject=0;
+
+const activeSlideProjects=n=>{
+    for (slideMobProject of slideMobProjects) {
+        slideMobProject.classList.remove('active');
+    }
+    slideMobProjects[n].classList.add('active');
+};
+const prepareCurrentSlideMobileProject=ind=>{
+    activeSlideProjects(indexMobProject);
+};
+const nextSlideMobileProject=()=>{
+    if (indexMobProject === slideMobProjects.length - 1){
+        indexMobProject=0;
+    }else{
+        indexMobProject++;
+    } prepareCurrentSlideMobileProject(indexMobProject);
+  };
+const prevSlideMobileProject=()=>{
+    if (indexMobProject === 0){
+        indexMobProject=slideMobProjects.length-1;
+    }else{
+        indexMobProject--;
+    }prepareCurrentSlideMobileProject(indexMobProject);
+};
+nextMobileProject.addEventListener('click',nextSlideMobileProject);
+prevMobileProject.addEventListener('click', prevSlideMobileProject);
+
+const prevMobileFantasies=document.getElementById('btn-prev-mobile-fantasies'),
+    nextMobileFantasies=document.getElementById('btn-next-mobile-fantasies'),
+    slideMobFantasies=document.querySelectorAll('.slide-mobile-fantasies');
+let indexMobFantasies=0;
+
+const activeSlideFantasies=n=>{
+    for (slideMobFantasy of slideMobFantasies) {
+        slideMobFantasy.classList.remove('active');
+    }
+    slideMobFantasies[n].classList.add('active');
+};
+const prepareCurrentSlideMobileFantasies=ind=>{
+    activeSlideFantasies(indexMobFantasies);
+};
+const nextSlideMobileFantasies=()=>{
+    if (indexMobFantasies === slideMobFantasies.length - 1){
+        indexMobFantasies=0;
+    }else{
+        indexMobFantasies++;
+    } prepareCurrentSlideMobileFantasies(indexMobFantasies);
+  };
+const prevSlideMobileFantasies=()=>{
+    if (indexMobFantasies === 0){
+        indexMobFantasies=slideMobFantasies.length-1;
+    }else{
+        indexMobFantasies--;
+    }prepareCurrentSlideMobileFantasies(indexMobFantasies);
+};
+nextMobileFantasies.addEventListener('click',nextSlideMobileFantasies);
+prevMobileFantasies.addEventListener('click', prevSlideMobileFantasies);
